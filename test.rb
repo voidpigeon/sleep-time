c90a2a89e67d0970a6104aa0d5b5f7d847f9a96a
voidpigeon <benheller42@gmail.com>
2016-05-11 21:41:38 -0700

    Only output time after interval

--- test.rb
+++ test.rb
@@ -1,20 +1,27 @@
-$last_action = Time.new
+$last_action = Time.new # who needs thread safety
+$prev_last_action = $last_action
+$recorded = true
+
+seconds_cutoff = 10
 
 def listen(path)
 	Thread.new do
 		File.open(path) do |file|
 			file.each_char do
 				$last_action = Time.new
+				$recorded = false
 			end
 		end
 	end
 end
 
 listen('/dev/input/mouse0')
-
 listen('/dev/input/event0')
 
 loop do
 	sleep 1
-	puts $last_action.strftime("%H:%M:%S")
+	if !$recorded && Time.new - $last_action > seconds_cutoff
+		$recorded = true
+		puts $last_action.strftime('%H:%M:%S')
+	end
 end
