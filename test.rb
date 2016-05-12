a8288d462bf0ef992907018f3b37abfe5f6956b8
voidpigeon <benheller42@gmail.com>
2016-05-11 23:05:05 -0700

    Add config file

--- test.rb
+++ test.rb
@@ -1,8 +1,24 @@
+def parse_config(str)
+	params = [:seconds_cutoff]
+	result = {}
+	for param in params
+		regex = /^\s*#{param.to_s}\s*=\s*(\d+)\s*$/
+		if str[regex]
+			result[param] = $1.to_i
+		end
+	end
+	result
+end
+
+def get_file(name)
+	File.join(__dir__, name)
+end
+
+config = parse_config(File.read(get_file("config.conf")))
+
 $last_action = Time.new # who needs thread safety
 $prev_last_action = $last_action
 $recorded = true
-
-seconds_cutoff = 10
 
 def listen(path)
 	Thread.new do
@@ -18,18 +34,14 @@ end
 listen('/dev/input/mouse0')
 listen('/dev/input/event0')
 
-def get_file(name)
-	File.join(__dir__, name)
-end
-
 loop do
 	sleep 1
-	if !$recorded && Time.new - $last_action > seconds_cutoff
+	if !$recorded && Time.new - $last_action > config[:seconds_cutoff]
 		$recorded = true
 		str = $last_action.strftime('%H:%M:%S') 
 		puts str
 		File.open(get_file('log.txt'), 'a') do |file|
-			file.puts str + '\n'
+			file.puts str
 		end
 	end
 end
