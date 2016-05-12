11cf9e14c8d7e4e17a80b72c058b89d90ace2aa6
voidpigeon <benheller42@gmail.com>
2016-05-11 20:46:38 -0700

    Track time of last input

--- test.rb
+++ test.rb
@@ -1,15 +1,20 @@
-def listen(path, &block)
-	File.open(path) do |file|
-		file.each_char(&block)
+$last_action = Time.new
+
+def listen(path)
+	Thread.new do
+		File.open(path) do |file|
+			file.each_char do
+				$last_action = Time.new
+			end
+		end
 	end
 end
 
-Thread.new do
-	listen('/dev/input/mouse0') do
-		puts 'mouse'
-	end
-end
+listen('/dev/input/mouse0')
 
-listen('/dev/input/event0') do
-	puts 'keyboard'
+listen('/dev/input/event0')
+
+loop do
+	sleep 1
+	puts $last_action.strftime("%H:%M:%S")
 end
